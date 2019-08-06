812e19f00d0f751ce33d57fa3f92d16d8b9f108b
Salami Usman Olawale <salamiusmanolawale@gmail.com>
2019-08-06 20:22:01 +0100

    Setting Up

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>MyBLOG</title>
+    <link rel="stylesheet" href="public/css/bootstrap.min.css">
+    <link rel="stylesheet" href="public/css/style.css">
+</head>
+<body>
+    <!-- include_once('./navbar/navbar.php') -->
+    <?php include './navbar/navbar.php';?>
+    
+    <script src="public/js/jquery-3.3.1.slim.min.js"></script>
+    <script src="public/js/popper.min.js"></script>
+    <script src="public/js/bootstrap.min.js"></script>
+</body>
+</html>
